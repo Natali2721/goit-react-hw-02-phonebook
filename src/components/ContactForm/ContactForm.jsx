046d1ab65f7f2a748744dfbd8225e8cfadd0101d
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-10-28 15:30:09 +0300

    Create ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import { Button, Form, Input, P } from './../Style/Element.styled';
+
+class ContactForm extends Component {
+  state = {};
+  render() {
+    return (
+      <Form>
+        <label>
+          <P>Name</P>
+
+          <Input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          <P>Number</P>
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <Button>Add contact</Button>
+      </Form>
+    );
+  }
+}
+export default ContactForm;

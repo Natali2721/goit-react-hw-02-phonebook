32fe18343527fc9b6d68ca501e03eedcf84ebfd0
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-10-28 15:32:35 +0300

    Create Filter.jsx

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,14 @@
+import { Input, P } from 'components/Style/Element.styled';
+
+export const Filter = ({ title }) => (
+  <div>
+    <P>{title}</P>
+    <Input
+      type="text"
+      name="nameToFind"
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      //      required
+    />
+  </div>
+);
